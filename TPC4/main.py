8702f8b7ac6ce8ca6d7f38ddfb70f98a08cfdbe9
Alexmartins01 <61992862+Alexmartins01@users.noreply.github.com>
2023-03-10 00:55:20 +0000

    works for variable lenght list

--- TPC4/main.py
+++ TPC4/main.py
@@ -36,20 +36,41 @@ def main():
             if line:
                 data = re.split(r',', line)
                 i = 0
+                while i < len(data):
+                    if data[i] == ', ' or data[i] == "":
+                        del data[i]
+                    else:
+                        i += 1
+                i = 0
+                print(data)
                 for campo in header:
                     if len(border) == 0 or int(border[0]) != i:
                         d.setdefault(campo, list())
                         d[campo].append(data[i])
                         i += 1
                     else:
+                        aux_list = list()
                         if len(border) == 2:
-                            aux_list = list()
                             for j in range(0, int(border[1])):
                                 aux_list.append(data[i+j])
                             d.setdefault(campo, list())
                             d[campo].append(aux_list)
                         if len(border) == 3:
-                            print("idk")
+                            for j in range(0, int(border[1])):
+                                aux_list.append(data[i + j])
+                            filled = len(header) + int(border[2]) - 1 - len(data)
+                            print(filled)
+                            print(border[1])
+                            if filled != 0:
+                                for j in range(int(border[1]), int(border[2]) - filled):
+                                    aux_list.append(data[i + j])
+                                d.setdefault(campo, list())
+                                d[campo].append(aux_list)
+                            else:
+                                for j in range(int(border[1]), int(border[2])):
+                                    aux_list.append(data[i + j])
+                                d.setdefault(campo, list())
+                                d[campo].append(aux_list)
     json_object = json.dumps(d, indent=4, ensure_ascii=False)
     encoded_output = json_object.encode('utf-8')
     decoded_output = encoded_output.decode('utf-8')
